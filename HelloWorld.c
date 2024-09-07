d43d7e5df3f89a6a4f0366eb8508cbfbc9596770
Sample-name-coder <harkiranpreet.k@atria.edu>
2024-09-07 11:45:46 +0530

    Added newlines

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("Add your name");
-   printf("My name is Sample");
+   printf("Hello, World!\n");
+   printf("Add your name\n");
+   printf("My name is Sample\n");
    return 0;
 }
